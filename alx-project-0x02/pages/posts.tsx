c289d031bfc80f6435450f9083a602b30ae50611
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-13 18:58:01 +0300

    Implement static data fetching in posts page with getStaticProps

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,26 +1,13 @@
-'use client';
-
-import { useEffect, useState } from 'react';
+import { GetStaticProps } from 'next';
 import PostCard from '@/components/common/PostCard';
 import { PostProps } from '@/interfaces';
 import Header from '@/components/layout/Header';
 
-const PostsPage = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-      .then(res => res.json())
-      .then(data => {
-        const formattedPosts = data.map((post: any) => ({
-          title: post.title,
-          content: post.body,
-          userId: post.userId,
-        }));
-        setPosts(formattedPosts);
-      });
-  }, []);
-
+const PostsPage = ({ posts }: PostsPageProps) => {
   return (
     <div className="min-h-screen p-4 bg-gray-50">
       <Header />
@@ -37,6 +24,23 @@ const PostsPage = () => {
       </div>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+  const data = await res.json();
+
+  const posts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default PostsPage;
