457992b6ff97f2022cb75a6a192cd16d37b65008
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-13 17:40:39 +0300

    Add reusable Card component and use it in home.tsx with different props

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Card from '../components/common/Card';
 
 export default function HomePage() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the Home Page</h1>
+      <Card title="Card One" content="This is the content of card one." />
+      <Card title="Card Two" content="This is the content of card two." />
+    </div>
+  );
 }
